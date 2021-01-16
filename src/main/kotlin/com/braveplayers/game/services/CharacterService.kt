252aca1f4464e96247dc12867460c2bb31daad84
Tiago Camilo <camilo.tiago@gmail.com>
2021-01-16 10:03:03 -0300

    Method create, changed name of param character -> entity

--- src/main/kotlin/com/braveplayers/game/services/CharacterService.kt
+++ src/main/kotlin/com/braveplayers/game/services/CharacterService.kt
@@ -3,7 +3,7 @@ package com.braveplayers.game.services
 import com.braveplayers.game.entities.Character
 
 interface CharacterService {
-    fun create(character: Character): Character
+    fun create(entity: Character): Character
     fun findById(id: Long): Character
     fun findAll(): Collection<Character>
     fun delete(id: Long): Character
