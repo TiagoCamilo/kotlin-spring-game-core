df579a7f3b588b2f37ac3bed3f9ee0318fc43652
Tiago Camilo <camilo.tiago@gmail.com>
2021-02-12 16:54:01 -0300

    Change annotation of DiscoveryClient

--- src/main/kotlin/com/braveplayers/game/GameApiApplication.kt
+++ src/main/kotlin/com/braveplayers/game/GameApiApplication.kt
@@ -2,9 +2,10 @@ package com.braveplayers.game
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 class GameApiApplication
 
