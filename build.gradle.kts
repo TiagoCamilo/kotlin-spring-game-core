6b2687c43b8f69c7f4bdf12ef0177e684dd60084
Tiago Camilo <camilo.tiago@gmail.com>
2021-02-12 13:17:17 -0300

    Add config server dependence

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,9 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-ui:1.5.2")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+    implementation("org.springframework.cloud:spring-cloud-starter-bootstrap")
+
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
