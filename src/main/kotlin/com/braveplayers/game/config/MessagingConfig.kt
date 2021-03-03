3bc92c4f85fe5ce834691c05959080aa03592b01
Tiago Camilo <camilo.tiago@gmail.com>
2021-03-03 16:44:26 -0300

    - Changed TopicExchange to DirectExchange - Removed extra configs

--- src/main/kotlin/com/braveplayers/game/config/MessagingConfig.kt
+++ src/main/kotlin/com/braveplayers/game/config/MessagingConfig.kt
@@ -1,8 +1,6 @@
 package com.braveplayers.game.config
 
 import org.springframework.amqp.core.*
-import org.springframework.amqp.rabbit.connection.ConnectionFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
 import org.springframework.amqp.support.converter.MessageConverter
 import org.springframework.beans.factory.annotation.Value
@@ -18,31 +16,25 @@ class MessagingConfig(
     @Value("\${messaging.routingkey.name}")
     val ROUTING_KEY: String,
 ) {
+
     @Bean
     fun queue(): Queue {
         return Queue(QUEUE)
     }
 
     @Bean
-    fun exchange(): TopicExchange {
-        return TopicExchange(EXCHANGE)
+    fun exchange(): DirectExchange {
+        return DirectExchange(EXCHANGE)
     }
 
     @Bean
-    fun binding(queue: Queue, exchange: TopicExchange): Binding {
+    fun binding(queue: Queue, exchange: DirectExchange): Binding {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY)
     }
 
     @Bean
     fun converter(): MessageConverter {
         return Jackson2JsonMessageConverter()
-    }
-
-    @Bean
-    fun template(connectionFactory: ConnectionFactory?): AmqpTemplate {
-        val rabbitTemplate = RabbitTemplate(connectionFactory!!)
-        rabbitTemplate.messageConverter = converter()
-        return rabbitTemplate
     }
 
 }
--- src/main/kotlin/com/braveplayers/game/producers/MessagingProducer.kt
+++ src/main/kotlin/com/braveplayers/game/producers/MessagingProducer.kt
@@ -3,17 +3,17 @@ package com.braveplayers.game.producers
 import com.braveplayers.game.config.MessagingConfig
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.core.AmqpTemplate
 import org.springframework.stereotype.Component
 
 @Component
-class MessagingProducer(val template: RabbitTemplate, val messagingConfig: MessagingConfig) {
+class MessagingProducer(val amqpTemplate: AmqpTemplate, val messagingConfig: MessagingConfig) {
 
     var logger: Logger = LoggerFactory.getLogger(MessagingProducer::class.java)
 
     fun send(content: Any) {
         try {
-            template.convertAndSend(messagingConfig.EXCHANGE, messagingConfig.ROUTING_KEY, content)
+            amqpTemplate.convertAndSend(messagingConfig.EXCHANGE, messagingConfig.ROUTING_KEY, content)
 
             logger.info("SEND MESSAGE: $content")
         } catch (e: Exception) {
